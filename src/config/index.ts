0f79636b57ff2df9f102c0e82ca302f292aaa31d
sathishkrishnan <sathishkrish20@gmail.com>
2024-01-21 09:48:32 +0530

    removed the api keys

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,3 @@
-export const API_URL = 'https://script.google.com/macros/s/AKfycbx5R_AuF5qQt18G7_YLiHN2hBlF-l3ZmBQBqRXCKUCMzFuepdbZkfKCXyJTlPXpblgZ/exec';
-
-export const API_KEY = 'AIzaSyDJJqLyXQS4B0Cw3NGmXHvB-H13Hw7LYGU';
 export const CLIENT_ID = '172108060298-8u42rovojaclj5shnfnhboc2phic45ot.apps.googleusercontent.com'
 export const SCOPES = 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/spreadsheets.readonly'
 
